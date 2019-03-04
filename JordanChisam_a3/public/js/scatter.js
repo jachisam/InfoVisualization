67c5ae25c8548ed20d1c41789a1e5c94a53284c8
Jordan Chisam <jachisam@me.com>
2019-03-04 16:31:53 -0600

    working on adding scatterplot svg to table

--- JordanChisam_a3/public/js/scatter.js
+++ JordanChisam_a3/public/js/scatter.js
@@ -4,7 +4,7 @@ d3.json("stories_dummy.json", function(data) {
     // console.log(data);
     scatterDict = transformData(data);
     appendStories(data.stories);
-    // scatterplot(data.stories);
+    scatterplot(data.stories);
 });
 
 function transformData(data){
@@ -21,33 +21,54 @@ function appendStories(data){
 
   var p = d3.select("#text-area").selectAll("td")
     .data(data)
-    .enter()
-    .append("tr")
-    .append("td")
+    .enter();
+  var tr = p.append("tr");
+  tr.append("td")
+    .text(function(d){
+      return 1;
+    });
+  tr.append("td")
     .text(function(d){
       return d.firstName;
     });
+  tr.append("td")
+    .text(function(d){
+      return d.ss;
+    });
+  tr.append("td")
+    .append("svg")
+    .attr("width", 250)
+    .attr("height", 25);
 }
 
 function scatterplot(data){
   console.log(data);
 
-  var svgContainer = d3.select("body").append("svg")
-    .attr("width", 700)
-    .attr("height", 700);
-
-  var circles = svgContainer.selectAll("circle")
+  var p = d3.select("#scatter_th").selectAll("tr")
     .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d, index){
-      console.log(d);
-      sentiment_score = d.ss;
-      return sentiment_score + (index*55);
-    })
-    .attr("cy", 25)
-    .attr("r", 25).
-    style("fill", "lightyellow");
+    .enter();
+    var tr = p.append("tr");
+    tr.append("td")
+      .text(function(d){
+        return 1;
+    });
+
+  // var svgContainer = d3.select("body").append("svg")
+  //   .attr("width", 700)
+  //   .attr("height", 700);
+  //
+  // var circles = svgContainer.selectAll("circle")
+  //   .data(data)
+  //   .enter()
+  //   .append("circle")
+  //   .attr("cx", function(d, index){
+  //     console.log(d);
+  //     sentiment_score = d.ss;
+  //     return sentiment_score + (index*55);
+  //   })
+  //   .attr("cy", 25)
+  //   .attr("r", 25).
+  //   style("fill", "lightyellow");
 }
 
 
